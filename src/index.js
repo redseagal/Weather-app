cabc2071107add41c4f9fe2d3346f20adef63b87
redseagal <70918566+redseagal@users.noreply.github.com>
2020-10-27 17:12:08 -0500

    Added JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-let apiKey = "6b639f16df2a6ce6f88ae77448e6832f";
+function displayTemperature(response) {
+  let temperatureELement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureELement.innerHTML = Math.round (response.date.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+
+let apiKey = "be29fb0c4ef8ab9821e7518352e71ee0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+
