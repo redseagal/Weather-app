04927560a84cff14aa704ec89188c01b858abdd2
redseagal <70918566+redseagal@users.noreply.github.com>
2020-10-28 13:05:43 -0500

    Added JS

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ function displayTemperature(response) {
   let temperatureELement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureELement.innerHTML = Math.round (response.date.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-
 let apiKey = "be29fb0c4ef8ab9821e7518352e71ee0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+//let apiKey = "be29fb0c4ef8ab9821e7518352e71ee0";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
