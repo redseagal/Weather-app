26b4c570a35732d8458eca6998eb0535dddc4420
redseagal <70918566+redseagal@users.noreply.github.com>
2020-10-28 15:09:32 -0500

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,51 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+}
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+
 function displayTemperature(response) {
   let temperatureELement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureELement.innerHTML = Math.round (response.date.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "be29fb0c4ef8ab9821e7518352e71ee0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-//let apiKey = "be29fb0c4ef8ab9821e7518352e71ee0";
+let apiKey = "6b639f16df2a6ce6f88ae77448e6832f";
+let city = "Chicago";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
 //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
 
